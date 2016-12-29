c49c287638c91ccce94e6e6dd834ae691c11d9a0
gotrecillo <sergio.panadero.perez@gmail.com>
2016-12-30 00:08:32 +0100

    added dummy test

---
+++ tests/api/DummyTestCest.php
@@ -0,0 +1,21 @@
+<?php
+
+
+class DummyTestCest
+{
+    public function _before(ApiTester $I)
+    {
+    }
+
+    public function _after(ApiTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(ApiTester $I)
+    {
+        $I->wantTo('Make a dummy test');
+        $I->sendGET('/');
+        $I->seeResponseCodeIs(200);
+    }
+}
